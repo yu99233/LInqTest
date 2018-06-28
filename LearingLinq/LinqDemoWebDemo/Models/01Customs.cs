9cb7f4e4b6479f5c13a2afb8de9615260a7e7fd5
yu99233 <15800305309@163.com>
2018-06-28 16:02:51 +0800

    DataContext

---
+++ LearingLinq/LinqDemoWebDemo/Models/01Customs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace LinqDemoWebDemo.Models
+{
+
+    //添加引用      System.Data.Linq.Mapping;
+
+    [Table(Name = "Customers")]
+    public class _01Customs
+    {
+        [Column(IsPrimaryKey = true)]
+        public string Id { get; set; }
+        [Column(Name = "ContactName")]
+        public string Name { get; set; }
+        [Column]
+        public string City { get; set; }
+    }
+}
